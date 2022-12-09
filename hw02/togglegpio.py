633037200f015c773059ce37e0cf9883b839d535
Sqzlnsy <sqz.lnsy@gmail.com>
2022-12-09 21:42:46 +0000

    Toggle GPIO as fast as possible using direct python

---
+++ hw02/togglegpio.py
@@ -0,0 +1,28 @@
+# !/usr/bin/python
+import time
+import os
+
+# Pin60
+pin="60"
+GPIOPATH='/sys/class/gpio/'
+
+if (not os.path.exists(GPIOPATH+"gpio"+pin)):
+    f=open(GPIOPATH+"export", "w")
+    f.write(pin)
+    f.close()
+
+f=open(GPIOPATH+"gpio"+pin+"/direction", "w")
+f.write("out")
+f.close()
+
+f=open(GPIOPATH+"gpio"+pin+"/value", "w")
+while True:
+    f.seek(0)
+    f.write("1")
+#    time.sleep(0.5)
+
+    f.seek(0)
+    f.write("0")
+#    time.sleep(0.5)
+
+f.close()
