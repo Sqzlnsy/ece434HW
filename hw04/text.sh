e32b16caad481a1520af9bbfed13237f26f80b08
Sqzlnsy <sqz.lnsy@gmail.com>
2023-01-08 17:29:36 +0000

    Mplay doesn't work

---
+++ hw04/text.sh
@@ -0,0 +1,14 @@
+# Here's how to use imagemagick to display text
+# Make a blank image
+SIZE=320x240
+TMP_FILE=/tmp/frame.png
+BACKGROUND=/home/debian/exercises/displays/demo/boris.png
+
+# From: http://www.imagemagick.org/Usage/text/
+convert -fill blue -font Times-Roman -pointsize 24 \
+     -size $SIZE \
+     label:'ImageMagick\nBoris.png\nby Jason Su' \
+     -draw "text 0,200 '01/08/2022'" \
+     $TMP_FILE
+composite -geometry $SIZE+150 $BACKGROUND $TMP_FILE $TMP_FILE     
+sudo fbi -noverbose -T 1 $TMP_FILE
